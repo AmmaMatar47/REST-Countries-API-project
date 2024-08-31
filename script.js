199915843135461e1eff84f92a3b7a3469320d4c
Ammar <raedamar52@gmail.com>
2024-09-01 00:59:45 +0300

    v1.0.2

--- script.js
+++ script.js
@@ -11,48 +11,58 @@ const search = document.querySelector(`.search-input`);
 const searchBtn = document.querySelector(`.search-btn`);
 const regionsContainer = document.querySelector('.region-options-container');
 const filterParagraph = document.querySelector('.filter');
+const loaderHTML = `<div class="loader"></div>`;
 const mainPage = document.querySelector('.main-page');
 const sectionCountry = document.querySelector('.details-section');
-const backBtn = document.querySelector(`.back-btn`);
-const neighbourContainerEl = document.querySelector('.neighbour-countries-container');
+const previewCountrySection = document.querySelector('.preview-section');
 const errorContainer = document.querySelector(`.error`);
 const errorText = document.querySelector(`.error-text`);
-
 ////////////////////////////////////////////////////////////////////
 // FETCH COUNTRIES FUNCTION , FILTERING AND SEARCH
 searchBtn.addEventListener('click', e => e.preventDefault());
+const timeout = delay =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => resolve(new Error('Request timeout try another time :)')), delay * 1000)
+  );
 
 const countryData = async function (type = 'all', typeInput = '', moreInfoPage = false) {
   try {
+    previewCountrySection.innerHTML = '';
+    main.innerHTML = '';
+    if (type === '/alpha') previewCountrySection.insertAdjacentHTML('afterbegin', loaderHTML);
+    else if (!main.querySelector('.loader')) main.insertAdjacentHTML('afterbegin', loaderHTML);
+
     errorContainer.classList.add('hidden-opacity');
-    const countriesFetch = await fetch(`https://restcountries.com/v3.1/${type}${typeInput}`);
-    const countriesData = await countriesFetch.json();
+    const response = await Promise.race([
+      fetch(`https://restcountries.com/v3.1/${type}${typeInput}`),
+      timeout(10),
+    ]);
+    if (response) {
+      response;
+    }
+    const countriesData = await response.json();
     if (moreInfoPage) displayCountryInfo(countriesData[0]);
     else {
       main.innerHTML = '';
       countriesData?.map(country => displayCountry(country));
     }
   } catch (error) {
+    console.error(error);
+    main.querySelector('.loader')?.remove();
     errorContainer.classList.remove('hidden-opacity');
     errorText.innerHTML = search.value;
   }
 };
-// DISPLAY All COUNTRIES
-countryData();
-
-main.addEventListener('click', e => {
-  if (e.target.closest('.country-card')) {
-    const countryName = e.target.closest('.country-card').dataset.card;
-    countryData('name/', `${countryName}?fullText=true`, true);
-  }
-});
 
 ////////////////////////////////////////////////////////////
 // DISPLAY COUNTRIES
 const displayCountry = function (country) {
   if (country.name.common === `Israel`) return;
   const formatPopulationToLocale = country.population.toLocaleString();
-  const countryCardHTML = `<div class="country-card" data-card="${country.name.common}">
+  const countryCardHTML = `<a href="#${('name ' + country.name.common).replaceAll(
+    ' ',
+    '_'
+  )}" class="country-card" data-card="${country.name.common}">
   <img src="${country.flags.png}" class="country-image" alt="${country.name.common} flag.";
 "/>
   <div class="country-info-container">
@@ -61,10 +71,9 @@ const displayCountry = function (country) {
   <p class="country-info region">Region: <span class="cards-info">${country.region}</span></p>
   <p class="country-info capital">Capital: <span class="cards-info">${country.capital}</span></p>
   </div>
-  </div>`;
+  </a>`;
   main.insertAdjacentHTML(`beforeend`, countryCardHTML);
 };
-
 ///////////////////////////////////////////////////////
 // FILTER BY REGION
 regionsContainer.addEventListener('click', function (e) {
@@ -88,40 +97,94 @@ search.addEventListener('input', e => {
 // Country details Information
 const displayCountryInfo = country => {
   const allLanguages = Object.values(country.languages);
-  const allCurrencies = Object.values(country.currencies).map(
-    currency => Object.values(currency)[0]
-  );
+  const allCurrencies = country.currencies
+    ? Object.values(country.currencies).map(currency => Object.values(currency)[0])
+    : 'No Currencies';
   const nativeName = Object.values(country.name.nativeName)[0].common;
 
   mainPage.classList.add('hidden');
-  sectionCountry.classList.remove('hidden');
-  backBtn.classList.remove('hidden');
-  backBtn.addEventListener('click', () => {
-    backBtn.classList.add('hidden');
-    sectionCountry.classList.add('hidden');
-    mainPage.classList.remove('hidden');
-  });
 
-  const flagImageEl = document.querySelector('.big-country-image');
+  let bordersCountries = '';
+  const previewCountryHTML = `<div class="padding-horizontally">
+      <button class="back-btn " name="Back">
+        <span class="left-arrow--icon">&larr;</span>
+        <span class="back-paragraph">Back</span>
+      </button>
+    </div>
 
-  flagImageEl.setAttribute(`src`, country.flags.png);
-  flagImageEl.alt = `${country.name.common} flag.`;
-  document.querySelector('h2[data-info-name]').innerHTML = country.name.common;
-  document.querySelector('span[data-info-name-native]').innerHTML = nativeName;
-  document.querySelector('span[data-info-population]').innerHTML =
-    country.population.toLocaleString();
-  document.querySelector('span[data-info-region]').innerHTML = country.region;
-  document.querySelector('span[data-info-sub-region]').innerHTML = country.subregion;
-  document.querySelector('span[data-info-capital]').innerHTML = country.capital;
-  document.querySelector('span[data-info-tld]').innerHTML = country.tld[0];
-  document.querySelector('span[data-info-cur]').innerHTML = allCurrencies;
-  document.querySelector('span[data-info-lng]').innerHTML = allLanguages;
-  neighbourContainerEl.innerHTML = '';
-  if (country.borders === undefined) neighbourContainerEl.innerHTML = `No neighbour countries.`;
-  country.borders?.map((border, index) => {
-    const bordersElement = `<button class="borders-btn" data-border="${country.borders[index]}">${border}</button>`;
-    neighbourContainerEl.insertAdjacentHTML('afterbegin', bordersElement);
-  });
+    <div class="details-section-container">
+      <section class="details-section padding-horizontally">
+        <img src="${country.flags.png}" class="big-country-image" alt="${
+    country.flags.alt || country.name.common + ' flag.'
+  }" />
+        <article class="info-section">
+          <div>
+            <h2 data-info-name="name">${country.name.common}</h2>
+          </div>
+          <div class="details-text-box">
+            <div>
+              <div class="info-headers">
+                <p class="sub-headers">Native Name:</p>
+                <span data-info-name-native="native-name">${nativeName}</span>
+              </div>
+              <div class="info-headers">
+                <p class="sub-headers">Population:</p>
+                <span data-info-population="population">${country.population.toLocaleString()}</span>
+              </div>
+              <div class="info-headers">
+                <p class="sub-headers">Region:</p>
+                <span data-info-region="region">${country.region}</span>
+              </div>
+              <div class="info-headers">
+                <p class="sub-headers">Sub Region:</p>
+                <span data-info-sub-region="sub-region">${country.subregion}</span>
+              </div>
+              <div class="info-headers">
+                <p class="sub-headers">Capital:</p>
+                <span data-info-capital="capital">${country.capital}</span>
+              </div>
+            </div>
+
+            <div>
+              <div class="info-headers">
+                <p class="sub-headers">Top Level Domain:</p>
+                <span data-info-tld="top-level-domain">${country.tld[0]}</span>
+              </div>
+              <div class="info-headers">
+                <p class="sub-headers">Currencies:</p>
+                <span data-info-cur="currencies">${allCurrencies}</span>
+              </div>
+              <div class="info-headers">
+                <p class="sub-headers">Languages:</p>
+                <span data-info-lng="languages">${allLanguages}</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="border-element-container">
+            <h3>Border Countries:</h3>
+            <div class="neighbour-countries-container">${
+              country.borders === undefined
+                ? `No neighbour countries.`
+                : country.borders
+                    .map((border, index, arr) => {
+                      if (border === 'ISR') return;
+                      const bordersElement = `<a href="#${border}" class="borders-btn" data-border="${country.borders[index]}">${border}</a>`;
+                      bordersCountries += bordersElement;
+                      if (index + 1 === arr.length) return bordersCountries;
+                    })
+                    .at(-1)
+            }</div>
+          </div>
+        </article>
+      </section>
+    </div>`;
+
+  previewCountrySection.innerHTML = '';
+  previewCountrySection.insertAdjacentHTML('afterbegin', previewCountryHTML);
+
+  const backBtn = document.querySelector(`.back-btn`);
+  backBtn.addEventListener('click', () => history.back());
 };
 
 ///////////////////////////////////////////////////////////////
@@ -129,7 +192,6 @@ const displayCountryInfo = country => {
 darkModeBtn.addEventListener('click', function () {
   const themeMode = root.getAttribute('theme');
   themeMode === 'light' ? root.setAttribute('theme', 'dark') : root.setAttribute('theme', 'light');
-
   moonIcon.setAttribute(`fill`, `hsl(0, 0%, 100%)`);
 });
 
@@ -143,10 +205,23 @@ regionFilterBtn.addEventListener('click', e => {
 });
 
 ////////////////////////////////////////////////////////
-// GO TO NEGHIBOUR COUNTRY
-neighbourContainerEl.addEventListener('click', e => {
-  if (e.target.classList.contains('borders-btn')) {
-    const borderCode = e.target.dataset.border;
-    countryData('alpha/', borderCode, true);
+// CHANGE ON URL
+
+window.addEventListener('popstate', () => {
+  if (location?.hash) {
+    location.hash.startsWith('#name')
+      ? countryData('name/', location.hash.replace('#', '').replaceAll('_', ' ').slice(5, -1), true)
+      : countryData('alpha/', location.hash.replace('#', ''), true);
+  } else {
+    // DISPLAY All COUNTRIES
+    mainPage?.children && countryData();
+    previewCountrySection.innerHTML = '';
+    mainPage.classList.remove('hidden');
   }
 });
+
+// WHEN PAGE LOADS
+if (location.hash.startsWith('#name'))
+  countryData('name/', location.hash.replace('#', '').replaceAll('_', ' ').slice(5, -1), true);
+else if (!location.hash) countryData();
+else countryData('alpha/', location.hash.replace('#', ''), true);
